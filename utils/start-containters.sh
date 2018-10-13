80b034f8f9431cffd11846b3579a0326f6adffcb
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2018-10-13 11:34:23 +0300

    adding more great containers

--- utils/start-containters.sh
+++ utils/start-containters.sh
@@ -1,3 +1,5 @@
+docker start syncthing
+docker start openvpn-as
 docker start nginx
 docker start mariadb
 docker start syncthing
@@ -5,5 +7,6 @@ docker start transmission
 docker start jackett
 docker start sonarr
 docker start radarr
+docker start zeronet
 docker start plex
 docker ps  
